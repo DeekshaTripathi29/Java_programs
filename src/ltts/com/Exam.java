46c65993bcf3b48802926cab573ed7509cc205a2
Deeksha Tripathi <deeksha18799@gmail.com>
2021-09-13 19:59:24 +0530

    my first commit

---
+++ src/ltts/com/Exam.java
@@ -0,0 +1,11 @@
+package ltts.com;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		System.out.println("Hii");
+		System.out.println("How are you?");
+
+	}
+
+}
--- src/ltts/com/Test.java
+++ src/ltts/com/Test.java
@@ -3,12 +3,11 @@ package ltts.com;
 public class Test {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
+		
 		System.out.println("Hello World");
 		System.out.println("Hii");
 		System.out.println("How is everything going on??");
-		System.out.println("All Good");
+		System.out.println("All good");
 	
 
 	}
