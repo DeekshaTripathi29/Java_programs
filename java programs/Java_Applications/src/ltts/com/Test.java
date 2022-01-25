75e41bac664642fb2405c3df058d4abbb2f3376f
Deeksha Tripathi <49842667+DeekshaTripathi29@users.noreply.github.com>
2022-01-25 19:55:21 +0530

    completed

---
+++ java programs/Java_Applications/src/ltts/com/Test.java
@@ -0,0 +1,15 @@
+package ltts.com;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Hii");
+		System.out.println("How is everything going on??");
+		System.out.println("All good");
+	
+
+	}
+
+}
